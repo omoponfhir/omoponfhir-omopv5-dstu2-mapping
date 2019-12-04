1af30acad37c00c365993eddbb22926b208beedb
Saul Crumpton <Bryan.Crumpton@gtri.gatech.edu>
2019-12-04 13:44:03 -0500

    filling in FHIRException.java

--- src/main/java/edu/gatech/chai/omoponfhir/omopv5/stu3/utilities/FHIRException.java
+++ src/main/java/edu/gatech/chai/omoponfhir/omopv5/stu3/utilities/FHIRException.java
@@ -1,4 +1,23 @@
 package edu.gatech.chai.omoponfhir.omopv5.stu3.utilities;
 
-public class FHIRException {
+public class FHIRException extends RuntimeException {
+
+    // Note that the 4-argument constructor has been removed as it is not JDK6 compatible
+
+    public FHIRException() {
+        super();
+    }
+
+    public FHIRException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FHIRException(String message) {
+        super(message);
+    }
+
+    public FHIRException(Throwable cause) {
+        super(cause);
+    }
+
 }

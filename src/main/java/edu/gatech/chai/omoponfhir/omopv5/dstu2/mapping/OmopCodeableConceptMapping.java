47876ab8cf2baddb7e5c68d65f5aa33afb1f8626
Saul Crumpton <saul.crumpton@gmail.com>
2020-07-15 14:15:31 -0400

    fixing missing semicolon

--- src/main/java/edu/gatech/chai/omoponfhir/omopv5/dstu2/mapping/OmopCodeableConceptMapping.java
+++ src/main/java/edu/gatech/chai/omoponfhir/omopv5/dstu2/mapping/OmopCodeableConceptMapping.java
@@ -40,7 +40,7 @@ public enum OmopCodeableConceptMapping {
 	NDFRT("http://hl7.org/fhir/ndfrt", "NDFRT"),
 	RXNORM("http://www.nlm.nih.gov/research/umls/rxnorm", "RxNorm"),
 	SCT("http://snomed.info/sct", "SNOMED"),
-	UCUM("http://unitsofmeasure.org", "UCUM"),
+	UCUM("http://unitsofmeasure.org", "UCUM");
 	
 	
 	public static String omopVocabularyforFhirUri(String fhirUri) throws FHIRException {
